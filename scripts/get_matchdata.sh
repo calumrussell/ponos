8e0498079aabf2650d89c85c366805d394c234da
calum <calum.mj.russell@gmail.com>
2023-10-05 16:16:17 +0100

    added echos to scripts

--- scripts/get_matchdata.sh
+++ scripts/get_matchdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for i in `ls data`; do 
+	echo "$i"
 	cat data/"$i"/matches | xargs -n 1 -d '\n' node xg-puppet/match.js > data/"$i"/match_data
 done
--- scripts/get_matches.sh
+++ scripts/get_matches.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for i in `ls data`; do 
+	echo "$i"
 	cat data/"$i"/seasons | xargs -n 1 node xg-puppet/fixtures.js > data/"$i"/matches
 done
