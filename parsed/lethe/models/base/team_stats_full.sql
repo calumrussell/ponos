3b57239b69d421ae76e7f261659484e92f54fdd6
calum <calum.mj.russell@gmail.com>
2023-10-17 17:30:44 +0100

    add xg to database views

--- parsed/lethe/models/base/team_stats_full.sql
+++ parsed/lethe/models/base/team_stats_full.sql
@@ -105,6 +105,10 @@
     "save_keeper"
 ] %}
 
+with xg_team as (
+    select sum(xg) as xg, match_id, team_id from {{ ref('player_stats_full') }} group by (match_id, team_id)
+)
+
 select 
     match.start_date,
     team_stats.team_id,
@@ -130,6 +134,7 @@ select
     case when opp_stats.clearance != 0 then opp_stats.clearance_effective::real/opp_stats.clearance::real else 0 end as opp_clearance_conversion,
     case when team_stats.big_chance_created != 0 then team_stats.big_chance_scored::real/team_stats.big_chance_created::real else 0.0::real end as big_chance_conversion,
     case when opp_stats.big_chance_created != 0 then opp_stats.big_chance_scored::real/opp_stats.big_chance_created::real else 0.0::real end as opp_big_chance_conversion,
+    CASE WHEN xg_team.xg is NULL THEN 0 ELSE xg_team.xg END AS xg,
     team.name as team,
     opp.name as opp
 from team_stats 
@@ -137,3 +142,4 @@ left join team_stats as opp_stats on team_stats.team_id=opp_stats.opp_id and tea
 left join team on team.id=team_stats.team_id
 left join team as opp on opp.id=team_stats.opp_id
 left join match on team_stats.match_id=match.id
+left join xg_team on xg_team.team_id=team_stats.team_id and xg_team.match_id=team_stats.match_id
