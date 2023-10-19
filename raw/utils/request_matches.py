500e6f869e0002bf9654da2fa105c5e3aed51725
calum <calum.mj.russell@gmail.com>
2023-10-19 17:02:15 +0100

    add bulk_matches adder script

--- raw/utils/request_matches.py
+++ raw/utils/request_matches.py
@@ -7,8 +7,11 @@ if __name__ == "__main__":
     for line in sys.stdin:
         row_json = json.loads(line)
         if row_json not in matches:
-            row_json['match_id'] = int(row_json['match_id'])
+            row_json['id'] = int(row_json['id'])
             row_json['home_id'] = int(row_json['home_id'])
             row_json['away_id'] = int(row_json['away_id'])
+            row_json['season_id'] = int(row_json['season_id'])
+            row_json['tournament_id'] = int(row_json['tournament_id'])
+            row_json['year'] = int(row_json['year'])
             matches.append(row_json)
-    res = requests.post('http://100.124.40.39:8080/bulk_input', json = {"matches": matches})
+    res = requests.post('http://localhost:8080/bulk_matches', json = {"matches": matches})
