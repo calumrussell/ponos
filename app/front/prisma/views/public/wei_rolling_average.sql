1cfe38bb885b02cc7ea6a26fae872ec41305f2f0
calum <calum.mj.russell@gmail.com>
2023-11-05 00:30:47 +0000

    update prisma for new views

---
+++ app/front/prisma/views/public/wei_rolling_average.sql
@@ -0,0 +1,37 @@
+SELECT
+  wei_ratings.team_id,
+  wei_ratings.date,
+  round(
+    avg((wei_ratings.off_rating) :: numeric) OVER (
+      PARTITION BY wei_ratings.team_id
+      ORDER BY
+        wei_ratings.date DESC ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+    ),
+    2
+  ) AS off_rating,
+  round(
+    avg((wei_ratings.off_rating_spread) :: numeric) OVER (
+      PARTITION BY wei_ratings.team_id
+      ORDER BY
+        wei_ratings.date DESC ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+    ),
+    2
+  ) AS off_rating_spread,
+  round(
+    avg((wei_ratings.def_rating) :: numeric) OVER (
+      PARTITION BY wei_ratings.team_id
+      ORDER BY
+        wei_ratings.date DESC ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+    ),
+    2
+  ) AS def_rating,
+  round(
+    avg((wei_ratings.def_rating_spread) :: numeric) OVER (
+      PARTITION BY wei_ratings.team_id
+      ORDER BY
+        wei_ratings.date DESC ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+    ),
+    2
+  ) AS def_rating_spread
+FROM
+  wei_ratings;
