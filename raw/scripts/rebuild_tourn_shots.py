74930035b4908b5c107794f7a79d04e229531fed
Calum Russell <calum.mj.russell@gmail.com>
2023-12-03 21:48:20 +0000

    added full rebuild shots, fixed full rebuild matches

--- raw/scripts/rebuild_tourn_shots.py
+++ raw/scripts/rebuild_tourn_shots.py
@@ -5,6 +5,7 @@ import psycopg2
 import subprocess
 import json
 import requests
+import ast
 
 if __name__ == "__main__":
     num = 50
@@ -19,13 +20,22 @@ if __name__ == "__main__":
             for offset in range(0, length + num, num):
                 print(offset)
                 cur.execute(f"select data from match_data where id in (select id from match where start_date < extract(epoch from now()) and tournament_id={tid}) limit {num} offset {offset};")
+
                 vals = "\n".join([json.dumps(row[0]) for row in cur.fetchall()])
                 process = subprocess.Popen(
-                        ['docker', 'run', '-i', '--rm', 'parser'], 
-                        stdin=subprocess.PIPE,
-                        stderr=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                        text=True)
+                            ['docker', 'run', '-i','--rm', 'pandora'], 
+                            stdin=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            stdout=subprocess.PIPE,
+                            text=True)
                 res, err = process.communicate(vals)
-                res = requests.post('http://100.96.98.54:8080/bulk_input', json = json.loads(res)) 
-                print(res.status_code)
+
+                values = ast.literal_eval(res)
+                if len(values) > 0:
+                    sql_values = ",".join(ast.literal_eval(res))
+                    sql_query = f"INSERT INTO xg(match_id, player_id, event_id, prob) VALUES {sql_values} on conflict(match_id, player_id, event_id) do update set prob=excluded.prob"
+                    cur = conn.cursor();
+                    cur.execute(sql_query)
+                    conn.commit()
+
+                print("Inserted: " + str(len(values)) + " shots")
